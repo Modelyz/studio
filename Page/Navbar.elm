3362e368ea594976c03864e54a66760dd4443646
Christophe Combelles <ccomb@free.fr>
2021-08-16 00:01:54 +0200

    operational processes at the top of navbar

--- Page/Navbar.elm
+++ Page/Navbar.elm
@@ -24,55 +24,47 @@ view model =
         , attribute "role" "navigation"
         , attribute "aria-label" "main navigation"
         ]
-        [ div
-            [ class "navbar-brand"
-            ]
-            [ a
-                [ classList
-                    [ ( "navbar-item", True )
-                    , ( "active", isActive model Route.ProcessTypes )
+        [ div [ class "navbar-brand" ]
+            [ div [class "navbar-item", class "has-dropdown", class "is-hoverable"]
+                <| [a [class "navbar-item"]
+                    [ text "Configuration"
+                    , div [class "navbar-dropdown"]
+                        [ a
+                            [ classList
+                                [ ( "navbar-item", True )
+                                , ( "active", isActive model Route.ProcessTypes )
+                                ]
+                            , href "/process-types"
+                            ]
+                            [ text "Process Types"
+                            ]
+                        , a
+                            [ classList
+                                [ ( "navbar-item", True )
+                                , ( "active", isActive model Route.EventTypes )
+                                ]
+                            , href "/event-types"
+                            ]
+                            [ text "Event Types"
+                            ]
+                        , a
+                            [ classList
+                                [ ( "navbar-item", True )
+                                , ( "active", isActive model Route.CommitmentTypes )
+                                ]
+                            , href "/commitment-types"
+                            ]
+                            [ text "Commitment Types"
+                            ]
+                        ]
                     ]
-                , href "/process-types"
-                ]
-                [ text "Process Types"
-                ]
-            , div
-                [ classList
-                    [ ( "navbar-item", True )
-                    , ( "has-dropdown", DictSet.size model.processTypes > 0)
-                    , ( "active", isActive model <| Route.Processes Nothing)
-                    , ("is-hoverable", True)
-                    ]
-                , href "#"
-                ]
-                [ a [class "navbar-item"]
-                    <| [text "Processes"]
-                         ++ case DictSet.size model.processTypes > 0 of
-                            True -> [ div [ class "navbar-dropdown" ]
-                                (model.processTypes
-                                    |> DictSet.toList
-                                    |> List.map (\pt -> a [class "navbar-item", href <| "/processes?type=" ++ pt.name] [text pt.name])
-                                )]
-                            False -> []
-                ]
-            , a
-                [ classList
-                    [ ( "navbar-item", True )
-                    , ( "active", isActive model Route.EventTypes )
-                    ]
-                , href "/event-types"
-                ]
-                [ text "Event Types"
-                ]
-            , a
-                [ classList
-                    [ ( "navbar-item", True )
-                    , ( "active", isActive model Route.CommitmentTypes )
-                    ]
-                , href "/commitment-types"
-                ]
-                [ text "Commitment Types"
-                ]
+            ] ++ case DictSet.size model.processTypes > 0 of
+                    True -> 
+                        model.processTypes
+                            |> DictSet.toList
+                            |> List.map (\pt -> a [class "navbar-item", href <| "/processes?type=" ++ pt.name] [text pt.name])
+                        
+                    False -> []
             , a
                 [ attribute "role" "button"
                 , class "navbar-burger"
@@ -85,4 +77,4 @@ view model =
                 , span [ attribute "aria-hidden" "true" ] []
                 ]
             ]
-        ]
+                ]
