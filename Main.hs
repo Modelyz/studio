ae79547326b8734f1ca56440a3a76645ef27a940
Christophe Combelles <ccomb@free.fr>
2021-09-26 23:16:31 +0200

    websocket connection that defaults to the http connection for static files

--- Main.hs
+++ Main.hs
@@ -9,6 +9,8 @@ import Data.List ()
 import qualified Data.ByteString as BS (pack, ByteString, append)
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import Data.Function ((&))
+import Network.Wai.Handler.WebSockets (websocketsOr)
+import Network.WebSockets (ServerApp, acceptRequest, sendTextData, defaultConnectionOptions)
 
 contentType :: T.Text -> T.Text
 contentType filename = case reverse $ T.split (=='.') filename of
@@ -17,8 +19,14 @@ contentType filename = case reverse $ T.split (=='.') filename of
     _ -> "raw"
 
 
-app :: Application
-app request respond = do
+wsApp :: ServerApp
+wsApp pending_conn = do
+        conn <- acceptRequest pending_conn
+        sendTextData conn ("Hello, client!" :: T.Text)
+
+
+httpApp :: Application
+httpApp request respond = do
     rawPathInfo request
         & decodeUtf8
         & T.append "Request "
@@ -37,6 +45,10 @@ app request respond = do
                 _ -> responseLBS status200 [("Content-Type", "text/html")] "static directory"
         _ -> responseFile status200 [("Content-Type", "text/html")] ("../pwa/src/templates/index.html"::String) Nothing
         
+
+app :: Application
+app = websocketsOr defaultConnectionOptions wsApp httpApp
+
 
 main :: IO ()
 main = do
