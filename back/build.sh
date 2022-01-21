91e395c2eeef8a10c3d53be2e5245be1b031191f
Christophe Combelles <ccomb@free.fr>
2022-01-21 23:26:34 +0100

    fixture to start the backend for selenium tests

--- back/build.sh
+++ back/build.sh
@@ -3,14 +3,20 @@
 BUILD="cabal build"
 OPTIMIZE=" -O2 --enable-split-objs --enable-executable-stripping --enable-library-stripping --enable-executable-dynamic"
 
+EXEC="dist-newstyle/build/x86_64-linux/ghc-8.8.4/ms-0.1.0.0/x/ms/$OPT/build/ms/ms"
+
 if [ "$1" == "-c" ]; then
     while true; do
         inotifywait -r -e modify -e create -e delete src/*hs src/*/*hs || killall less &
         sleep 0.1
 	$BUILD
+	cp -a $EXEC ../build/
     done
 elif [ "$1" == "-o" ]; then
+    OPT="opt"
     $BUILD $OPTIMIZE
+    cp -af $EXEC ../build/
 else
     $BUILD
+    cp -af $EXEC ../build/
 fi
--- test/test.py
+++ test/test.py
@@ -9,15 +9,20 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 import subprocess
 import pytest
+import time
 
 
 @pytest.fixture
-def run_backend():
-    backend = subprocess.run(["./run.sh"], cwd="../back")
+def backend():
+    backend = subprocess.Popen(["../build/ms"])
+    time.sleep(0.2)
     yield backend
+    backend.terminate()
+    backend.wait()
 
 
-def test_chrome():
+def test_chrome(backend):
+    assert backend.poll() is None, "Backend is not running"
     service = ChromeService(executable_path=ChromeDriverManager().install())
 
     driver = webdriver.Chrome(service=service)
@@ -25,7 +30,8 @@ def test_chrome():
     driver.quit()
 
 
-def test_firefox():
+def test_firefox(backend):
+    assert backend.poll() is None, "Backend is not running"
     service = FirefoxService(executable_path=GeckoDriverManager().install())
 
     driver = webdriver.Firefox(service=service)
