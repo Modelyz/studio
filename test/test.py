ed8e7918e4e05b8fab4687a47d5d05c01e8139af
Christophe Combelles <ccomb@free.fr>
2022-01-22 12:42:40 +0100

    selenium setup

--- test/test.py
+++ test/test.py
@@ -2,6 +2,8 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,31 +11,50 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 import subprocess
 import pytest
-import time
+import os
 
 
 @pytest.fixture
 def backend():
-    backend = subprocess.Popen(["../build/ms"])
-    time.sleep(0.2)
-    yield backend
-    backend.terminate()
-    backend.wait()
+    if os.path.exists("eventstore.txt"):
+        os.remove("eventstore.txt")
+    with subprocess.Popen(["../build/ms"]) as proc:
+        proc.is_running = lambda: proc.poll() is None
+        yield proc
+        proc.terminate()
 
 
-def test_chrome(backend):
-    assert backend.poll() is None, "Backend is not running"
-    service = ChromeService(executable_path=ChromeDriverManager().install())
+def driver(nav):
+    if nav == "chrome":
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+    elif nav == "firefox":
+        service = FirefoxService(executable_path=GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service)
+    else:
+        raise NotImplementedError
+    return driver
 
-    driver = webdriver.Chrome(service=service)
 
-    driver.quit()
+def test(backend):
 
+    chrome = driver("chrome")
+    chrome.implicitly_wait(5)
 
-def test_firefox(backend):
-    assert backend.poll() is None, "Backend is not running"
-    service = FirefoxService(executable_path=GeckoDriverManager().install())
+    assert backend.is_running(), "Backend stopped"
+    chrome.get("http://localhost:8080/process-types")
+    assert chrome.title == "Process Types"
 
-    driver = webdriver.Firefox(service=service)
+    assert backend.is_running(), "Backend stopped"
+    chrome.find_element(By.CLASS_NAME, "input").send_keys("New PT" + Keys.ENTER)
+    box = chrome.find_element(By.CLASS_NAME, "box")
+    assert box.text == "New PT"
 
-    driver.quit()
+    chrome.quit()
+
+    assert backend.is_running(), "Backend stopped"
+    firefox = driver("firefox")
+    firefox.implicitly_wait(5)
+    firefox.get("http://localhost:8080/process-types")
+    assert firefox.title == "Process Types"
+    firefox.quit()
