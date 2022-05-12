718e0556ec734c3da1b6c4d3a0a105ebc4fbfb0b
Christophe Combelles <ccomb@free.fr>
2022-05-12 18:59:37 +0200

    fixed front build

--- front/build.sh
+++ front/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
 pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ "$1" == "-o" ]; then
     rsync -r --delete src/static/ ../build/static/
+    cp -f src/index.html ../build
     elm make --optimize --output ../build/tmp.js src/Main.elm \
         && uglifyjs ../build/tmp.js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
         | uglifyjs --mangle --output ../build/static/app.js \
     && rm ../build/tmp.js
 else
     rsync -r --delete src/static/ ../build/static/
+    cp -f src/index.html ../build
     elm make --output ../build/static/app.js src/Main.elm
 fi
 popd
