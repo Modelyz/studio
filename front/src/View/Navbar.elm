7874aca01e0547fed23e07d28aa5ea74a9fbaae1
Christophe Combelles <ccomb@free.fr>
2022-05-17 10:43:10 +0200

    moved debug info

--- front/src/View/Navbar.elm
+++ front/src/View/Navbar.elm
@@ -99,8 +99,8 @@ desktop s r =
                         , el [ Font.size 15, htmlAttribute <| Attr.id "timeoutReconnect" ] (text <| "timeoutReconnect=" ++ (String.fromInt <| s.timeoutReconnect))
                         , el [ Font.size 15, htmlAttribute <| Attr.id "pending" ] (text <| "pending=" ++ (String.fromInt <| Set.size s.state.pendingEvents))
                         , el [ Font.size 15, htmlAttribute <| Attr.id "msgs" ] (text <| "msgs=" ++ (String.fromInt <| Set.size s.state.uuids))
+                        , el [ Font.size 15, htmlAttribute <| Attr.id "msgs" ] (text <| "Route=" ++ toString r)
                         ]
-                   , el [ Font.size 15, htmlAttribute <| Attr.id "msgs" ] (text <| "Route=" ++ toString r)
                    ]
 
 
