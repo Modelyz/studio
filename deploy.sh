7dd9857fd468ea5d84738607dc9ab2ada2644b39
Christophe Combelles <ccomb@free.fr>
2022-05-19 08:14:35 +0200

    optional build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+
+if [ "$1" == "--build" ]; then
+   build="--build"
+else
+    build=""
+fi
+
 # define studio host in .ssh/config
-ssh studio "cd services/studio && git pull && /opt/bin/docker-compose up -d --build"
+ssh studio "cd services/studio && git pull && /opt/bin/docker-compose up -d $build"
