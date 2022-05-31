fddfc49f8c7145ed9b16fe86635fb4f75b66f9b6
Christophe Combelles <ccomb@free.fr>
2022-05-31 14:26:22 +0200

    no build option

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-
-if [ "$1" == "--build" ]; then
-   build="/opt/bin/docker-compose build"
-else
-    build="echo \"(skipping build)\""
-fi
-
 # define studio host in .ssh/config
-ssh studio "cd services/studio && git pull && $build && /opt/bin/docker-compose up -d"
+ssh studio "cd services/studio && git pull && /opt/bin/docker-compose build && /opt/bin/docker-compose up -d"
