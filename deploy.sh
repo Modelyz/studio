84df3a5dcc9616d9b7edd06c55bfa74559a363e4
Christophe Combelles <ccomb@free.fr>
2022-05-31 11:51:57 +0200

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 
 if [ "$1" == "--build" ]; then
-   build="--build"
+   build="/opt/bin/docker-compose build"
 else
-    build=""
+    build="echo \"(skipping build)\""
 fi
 
 # define studio host in .ssh/config
-ssh studio "cd services/studio && git pull && /opt/bin/docker-compose up -d $build"
+ssh studio "cd services/studio && git pull && $build && /opt/bin/docker-compose up -d"
