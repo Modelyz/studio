ef8256c17543e2937bc97e0896cdb9f8be55ae2a
Christophe Combelles <ccomb@free.fr>
2022-07-16 12:54:00 +0200

    simpler build

--- back/build.sh
+++ back/build.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
+INSTALLDIR=../build
 set -e
-BUILD="cabal build"
-DEVEL='--ghc-options="-Wall"'
-OPTIMIZE=" -O2 --enable-split-objs --enable-executable-stripping --enable-library-stripping --enable-executable-static"
-DIR="dist-newstyle/build/x86_64-linux/"
 
 pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 mkdir -p ../build
 
 if [ "$1" == "-o" ]; then
-    $BUILD $OPTIMIZE
-    find $DIR -type f -name studio -exec cp {} ../build/ \;
+    # TODO stripping does not work
+    cabal install -O2 --enable-split-objs --enable-executable-static --enable-executable-stripping --install-method=copy --installdir=$INSTALLDIR --overwrite-policy=always
+    #cabal v1-install -O2 --enable-split-objs --enable-executable-static --enable-executable-stripping --bindir=$INSTALLDIR
 else
-    $BUILD $DEVEL
-    find $DIR -type f -name studio -exec cp {} ../build/ \;
+    cabal install --ghc-options="-Wall" --installdir=$INSTALLDIR --overwrite-policy=always
 fi
 popd
