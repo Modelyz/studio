e2dcc1801d890e02d2c9eabd58c55b6b4800042b
Christophe Combelles <ccomb@free.fr>
2022-07-26 11:47:17 +0200

    deploy dir

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # define studio host in .ssh/config
-ssh studio "cd services/studio && git pull && /opt/bin/docker-compose build && /opt/bin/docker-compose up -d"
+ssh studio "cd services/modelyz && git pull && /opt/bin/docker-compose build && /opt/bin/docker-compose up -d"
