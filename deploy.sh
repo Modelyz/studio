75ecb87084c336cf68f4e820b0a8096b0b4e0676
Christophe Combelles <ccomb@free.fr>
2022-07-26 11:58:37 +0200

    reset option for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+RESET="echo keeping data"
+if [ "$1" == "--reset" ]; then
+    echo "Deleting data"
+    RESET="/opt/bin/docker-compose down -v"
+fi
+
 # define studio host in .ssh/config
-ssh studio "cd services/modelyz && git pull && /opt/bin/docker-compose build && /opt/bin/docker-compose up -d"
+ssh studio "cd services/modelyz && $RESET && git pull && /opt/bin/docker-compose build && /opt/bin/docker-compose up -d"
