fe2ba1d9fd67877214533f592ab848b5d1a359e9
Christophe Combelles <ccomb@free.fr>
2022-08-25 16:20:15 +0200

    adapt group mngmt to AgentType

--- front/src/AgentType/AddPage.elm
+++ front/src/AgentType/AddPage.elm
@@ -44,6 +44,7 @@ type alias Model =
     , seed : Seed
     , flatselect : Maybe AgentType
     , identifiers : Dict String Identifier
+    , oldGroups : Dict String Group
     , groups : Dict String Group
     , warning : String
     , step : Step.Step Step
@@ -98,6 +99,12 @@ init s f =
         |> Maybe.andThen (H.find s.state.agentTypes)
         |> Maybe.map
             (\at ->
+                let
+                    oldGroups =
+                        s.state.grouped
+                            |> Dict.filter (\_ v -> at.uuid == Groupable.uuid v.groupable)
+                            |> Dict.foldl (\_ v d -> Dict.insert (Group.compare v.group) v.group d) Dict.empty
+                in
                 { route = f.route
                 , flatselect = at.parent |> Maybe.andThen (H.find s.state.agentTypes)
                 , uuid = at.uuid
@@ -105,7 +112,8 @@ init s f =
                 , identifiers =
                     initIdentifiers s.state.agents s.state.agentTypes s.state.identifierTypes (Type.HType HType.AgentType) Nothing at.uuid
                         |> Dict.union (Identifier.fromUuid at.what at.uuid s.state.identifiers)
-                , groups = Dict.empty
+                , oldGroups = oldGroups
+                , groups = oldGroups
                 , warning = ""
                 , step = Step.Step StepType
                 , steps = [ Step.Step StepType, Step.Step StepIdentifiers, Step.Step StepGroups ]
@@ -117,6 +125,7 @@ init s f =
             , uuid = newUuid
             , seed = newSeed
             , identifiers = initIdentifiers s.state.agents s.state.agentTypes s.state.identifierTypes (Type.HType HType.AgentType) Nothing newUuid
+            , oldGroups = Dict.empty
             , groups = Dict.empty
             , warning = ""
             , step = Step.Step StepType
@@ -150,12 +159,20 @@ update s msg model =
         Added ->
             case validate model of
                 Ok at ->
+                    let
+                        addedGroups =
+                            Dict.diff model.groups model.oldGroups
+
+                        removedGroups =
+                            Dict.diff model.oldGroups model.groups
+                    in
                     ( model
                     , Effect.batch
                         [ Shared.dispatchMany s
                             (Message.AddedAgentType at
                                 :: List.map Message.IdentifierAdded (Dict.values model.identifiers)
-                                ++ List.map (\g -> Message.Grouped (Groupable.AT at) g) (Dict.values model.groups)
+                                ++ List.map (\g -> Message.Grouped (Groupable.AT at) g) (Dict.values addedGroups)
+                                ++ List.map (\g -> Message.Ungrouped (Groupable.AT at) g) (Dict.values removedGroups)
                             )
                         , redirectParent s.navkey model.route |> Effect.fromCmd
                         ]
