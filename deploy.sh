7900b8ea8b511be3b42e1e367d07d7400b4de456
Christophe Combelles <ccomb@free.fr>
2022-09-20 15:05:27 +0200

    improve build

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-RESET="echo keeping data"
 if [ "$1" == "--reset" ]; then
     echo "Deleting data"
     RESET="/opt/bin/docker-compose down -v"
+elif [ "$1" == "--reset" ]; then
+    RESET="echo keeping data"
+else
+    echo use --reset to delete data or --keep to keep it
+    exit 1
 fi
 
 # define studio host in .ssh/config
--- front/build.sh
+++ front/build.sh
@@ -9,12 +9,17 @@ if [[ -z "${WSS}" ]]; then
     export WSS="ws://localhost:8080"
 fi
 
+# change to the dir of this script
 pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# don't build if the changelog is not up to date
 grep "## version ${APPVERSION} --" ../CHANGELOG.md \
     || { echo "Please first feed the changelog for version ${APPVERSION}"; exit 1; }
 
+# update the static dir
 rsync -r --delete src/static/ ../build/static/
 
+# build
 if [ "$1" == "-o" ]; then
     elm make --optimize --output ../build/tmp.js src/Main.elm \
         && uglifyjs ../build/tmp.js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
@@ -27,8 +32,10 @@ else
     export APP=app
 fi
 
+# build the index
 envsubst < src/index.html > ../build/index.html
 
+# generate the changelog
 markdown ../CHANGELOG.md > ../build/static/changelog.html
 
 popd
