12ff7e962f4e7b724e87e5b582623247a2621ea3
Christophe Combelles <ccomb@free.fr>
2022-10-13 10:30:28 +0200

    deploy option to keep data

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "--reset" ]; then
     echo "Deleting data"
     RESET="/opt/bin/docker-compose down -v"
-elif [ "$1" == "--reset" ]; then
+elif [ "$1" == "--keep" ]; then
     RESET="echo keeping data"
 else
     echo use --reset to delete data or --keep to keep it
