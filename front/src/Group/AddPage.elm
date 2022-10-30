f62a16e999413b3e7b58b1f65afdc615945a47b8
Christophe Combelles <ccomb@free.fr>
2022-10-30 22:19:15 +0100

    Fixed type not displayed in the EditPage

--- front/src/Group/AddPage.elm
+++ front/src/Group/AddPage.elm
@@ -245,6 +245,7 @@ viewContent model s =
                         [ wrappedRow [ width <| minimum 50 shrink, Border.width 2, padding 3, spacing 4, Border.color color.item.border ] <|
                             [ h2 "Type"
                             , model.flatselect
+                                |> Maybe.map (hWithIdentifiers (allT s) (allH s) s.state.identifierTypes s.state.identifiers)
                                 |> Maybe.map (hViewHalfCard (InputType Nothing) (allT s) allHwithIdentifiers s.state.configs)
                                 |> Maybe.withDefault (el [ padding 5, Font.color color.text.disabled ] (text "Empty"))
                             ]
