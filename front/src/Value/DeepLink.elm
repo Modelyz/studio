af7257ecedc134759e3929edebaa94feb93a20bb
Christophe Combelles <ccomb@free.fr>
2022-11-11 21:25:39 +0100

    let values appears in the deeplink construction subpage

--- front/src/Value/DeepLink.elm
+++ front/src/Value/DeepLink.elm
@@ -100,20 +100,21 @@ view s model =
                 EndPoint value ->
                     []
         ]
-            ++ [ selectValue s model (dlToScope model model.deeplink) (Selected << EndPoint) ]
+            ++ [ selectValue s model (Debug.log "Scope" (dlToScope model.scope (Debug.log "deeplink" model.deeplink))) (Selected << EndPoint) ]
 
 
-dlToScope : Model -> DeepLink -> Scope
-dlToScope model deeplink =
+dlToScope : Scope -> DeepLink -> Scope
+dlToScope scope deeplink =
+    -- TODO pass the restriction scope on the hardlink to narrow the choice of values
     case deeplink of
         Null ->
-            model.scope
+            scope
 
         Link hl dl ->
-            dlToScope model dl
+            dlToScope (hlToScope hl) dl
 
         EndPoint v ->
-            model.scope
+            scope
 
 
 selectValue : Shared.Model -> Model -> Scope -> (Value -> Msg) -> Element Msg
@@ -127,7 +128,31 @@ selectValue s model scope onInput =
                         |> Dict.values
                         |> List.filter (\v -> v.what == t && v.for == uuid)
                         |> List.map (\v -> clickableCard (onInput v) (text v.name) none)
-                        |> withDefaultContent (text "(No values are defined for this entity. Choose another entity or create a value for this entity)")
+                        |> withDefaultContent (text "(No values are defined for this scope. Choose another entity or create a value for this entity)")
+                    )
+                ]
+
+        HasUserType t ht _ ->
+            column [ spacing 20 ]
+                [ h2 "Select the value you want to choose:"
+                , wrappedRow [ padding 10, spacing 10, Border.color color.item.border ]
+                    (s.state.values
+                        |> Dict.values
+                        |> List.filter (\v -> v.what == Type.HType ht)
+                        |> List.map (\v -> clickableCard (onInput v) (text v.name) none)
+                        |> withDefaultContent (text "(No values are defined for this scope. Choose another entity or create a value for this entity)")
+                    )
+                ]
+
+        HasType t ->
+            column [ spacing 20 ]
+                [ h2 "Select the value you want to choose:"
+                , wrappedRow [ padding 10, spacing 10, Border.color color.item.border ]
+                    (s.state.values
+                        |> Dict.values
+                        |> List.filter (\v -> v.what == t)
+                        |> List.map (\v -> clickableCard (onInput v) (text v.name) none)
+                        |> withDefaultContent (text "(No values are defined for this scope. Choose another entity or create a value for this entity)")
                     )
                 ]
 
@@ -184,3 +209,238 @@ scopeToChoice scope =
 
         _ ->
             []
+
+
+hlToScope : HardLink -> Scope
+hlToScope x =
+    case x of
+        HL.ResourceLink y ->
+            rlToScope y
+
+        HL.EventLink y ->
+            elToScope y
+
+        HL.AgentLink y ->
+            alToScope y
+
+        HL.CommitmentLink y ->
+            cmlToScope y
+
+        HL.ContractLink y ->
+            cnlToScope y
+
+        HL.ProcessLink y ->
+            plToScope y
+
+        HL.GroupLink y ->
+            glToScope y
+
+        HL.ResourceTypeLink y ->
+            rtlToScope y
+
+        HL.EventTypeLink y ->
+            etlToScope y
+
+        HL.AgentTypeLink y ->
+            atlToScope y
+
+        HL.CommitmentTypeLink y ->
+            cmtlToScope y
+
+        HL.ContractTypeLink y ->
+            cntlToScope y
+
+        HL.ProcessTypeLink y ->
+            ptlToScope y
+
+        HL.GroupTypeLink y ->
+            gtlToScope y
+
+
+rlToScope : HL.ResourceLink -> Scope
+rlToScope x =
+    case x of
+        HL.ResourceGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.ResourceType ->
+            HasType (Type.HType HType.ResourceType)
+
+
+elToScope : HL.EventLink -> Scope
+elToScope x =
+    case x of
+        HL.EventProvider ->
+            HasType (Type.TType TType.Agent)
+
+        HL.EventReceiver ->
+            HasType (Type.TType TType.Agent)
+
+        HL.EventInflow ->
+            HasType (Type.TType TType.Resource)
+
+        HL.EventOutflow ->
+            HasType (Type.TType TType.Resource)
+
+        HL.EventGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.EventType ->
+            HasType (Type.HType HType.EventType)
+
+
+alToScope : HL.AgentLink -> Scope
+alToScope x =
+    case x of
+        HL.AgentGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.AgentType ->
+            HasType (Type.HType HType.AgentType)
+
+
+cmlToScope : HL.CommitmentLink -> Scope
+cmlToScope x =
+    case x of
+        HL.CommitmentProvider ->
+            HasType (Type.TType TType.Agent)
+
+        HL.CommitmentReceiver ->
+            HasType (Type.TType TType.Agent)
+
+        HL.CommitmentInflow ->
+            HasType (Type.TType TType.Resource)
+
+        HL.CommitmentOutflow ->
+            HasType (Type.TType TType.Resource)
+
+        HL.CommitmentGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.CommitmentType ->
+            HasType (Type.HType HType.CommitmentType)
+
+
+cnlToScope : HL.ContractLink -> Scope
+cnlToScope x =
+    case x of
+        HL.ContractGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.ContractType ->
+            HasType (Type.HType HType.ContractType)
+
+
+plToScope : HL.ProcessLink -> Scope
+plToScope x =
+    case x of
+        HL.ProcessGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.ProcessType ->
+            HasType (Type.HType HType.ProcessType)
+
+
+glToScope : HL.GroupLink -> Scope
+glToScope x =
+    case x of
+        HL.GroupGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.GroupType ->
+            HasType (Type.HType HType.GroupType)
+
+
+rtlToScope : HL.ResourceTypeLink -> Scope
+rtlToScope x =
+    case x of
+        HL.ResourceTypeGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.ResourceTypeParent ->
+            HasType (Type.HType HType.ResourceType)
+
+
+etlToScope : HL.EventTypeLink -> Scope
+etlToScope x =
+    case x of
+        HL.EventTypeProvider ->
+            HasType (Type.TType TType.Agent)
+
+        HL.EventTypeReceiver ->
+            HasType (Type.TType TType.Agent)
+
+        HL.EventTypeInflow ->
+            -- TODO an inflow can also defined by be a RT
+            HasType (Type.TType TType.Resource)
+
+        HL.EventTypeOutflow ->
+            HasType (Type.TType TType.Resource)
+
+        HL.EventTypeGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.EventTypeParent ->
+            HasType (Type.HType HType.EventType)
+
+
+atlToScope : HL.AgentTypeLink -> Scope
+atlToScope x =
+    case x of
+        HL.AgentTypeGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.AgentTypeParent ->
+            HasType (Type.HType HType.AgentType)
+
+
+cmtlToScope : HL.CommitmentTypeLink -> Scope
+cmtlToScope x =
+    case x of
+        HL.CommitmentTypeProvider ->
+            HasType (Type.TType TType.Agent)
+
+        HL.CommitmentTypeReceiver ->
+            HasType (Type.TType TType.Agent)
+
+        HL.CommitmentTypeInflow ->
+            HasType (Type.TType TType.Resource)
+
+        HL.CommitmentTypeOutflow ->
+            HasType (Type.TType TType.Resource)
+
+        HL.CommitmentTypeGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.CommitmentTypeParent ->
+            HasType (Type.HType HType.CommitmentType)
+
+
+cntlToScope : HL.ContractTypeLink -> Scope
+cntlToScope x =
+    case x of
+        HL.ContractTypeGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.ContractTypeParent ->
+            HasType (Type.HType HType.ContractType)
+
+
+ptlToScope : HL.ProcessTypeLink -> Scope
+ptlToScope x =
+    case x of
+        HL.ProcessTypeGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.ProcessTypeParent ->
+            HasType (Type.HType HType.ProcessType)
+
+
+gtlToScope : HL.GroupTypeLink -> Scope
+gtlToScope x =
+    case x of
+        HL.GroupTypeGroup ->
+            HasType (Type.TType TType.Group)
+
+        HL.GroupTypeParent ->
+            HasType (Type.HType HType.GroupType)
