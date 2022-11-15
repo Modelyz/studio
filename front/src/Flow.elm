7e42786952e62a424fcfecef793507527b9d6784
Christophe Combelles <ccomb@free.fr>
2022-11-15 17:58:48 +0100

    In or out flow is already determined by the provider/receiver relationship

--- front/src/Flow.elm
+++ front/src/Flow.elm
@@ -8,39 +8,23 @@ import Value.Rational as Rational exposing (Rational(..))
 
 
 type Flow
-    = Inflow Rational Resource
-    | TypeInflow Rational ResourceType
-    | Outflow Rational Resource
-    | TypeOutflow Rational ResourceType
+    = RFlow Rational Resource
+    | RTFlow Rational ResourceType
 
 
 encode : Flow -> Encode.Value
 encode flow =
     case flow of
-        Inflow qty resource ->
+        RFlow qty resource ->
             Encode.object
                 [ ( "type", Encode.string "Inflow" )
                 , ( "qty", Rational.encode qty )
                 , ( "resource", Resource.encode resource )
                 ]
 
-        TypeInflow qty resourceType ->
+        RTFlow qty resourceType ->
             Encode.object
                 [ ( "type", Encode.string "TypeInflow" )
-                , ( "qty", Rational.encode qty )
-                , ( "resourceType", ResourceType.encode resourceType )
-                ]
-
-        Outflow qty resource ->
-            Encode.object
-                [ ( "type", Encode.string "*Outflow" )
-                , ( "qty", Rational.encode qty )
-                , ( "resource", Resource.encode resource )
-                ]
-
-        TypeOutflow qty resourceType ->
-            Encode.object
-                [ ( "type", Encode.string "TypeOutflow" )
                 , ( "qty", Rational.encode qty )
                 , ( "resourceType", ResourceType.encode resourceType )
                 ]
@@ -52,23 +36,13 @@ decoder =
         |> Decode.andThen
             (\t ->
                 case t of
-                    "Inflow" ->
-                        Decode.map2 Inflow
+                    "RFlow" ->
+                        Decode.map2 RFlow
                             (Decode.field "qty" Rational.decoder)
                             (Decode.field "resource" Resource.decoder)
 
-                    "TypeInflow" ->
-                        Decode.map2 TypeInflow
-                            (Decode.field "qty" Rational.decoder)
-                            (Decode.field "resourceType" ResourceType.decoder)
-
-                    "Outflow" ->
-                        Decode.map2 Outflow
-                            (Decode.field "qty" Rational.decoder)
-                            (Decode.field "resource" Resource.decoder)
-
-                    "TypeOutflow" ->
-                        Decode.map2 TypeOutflow
+                    "RTFlow" ->
+                        Decode.map2 RTFlow
                             (Decode.field "qty" Rational.decoder)
                             (Decode.field "resourceType" ResourceType.decoder)
 
