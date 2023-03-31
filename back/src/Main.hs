ec1a469ac0053a89a3d084f4ca340900bbc5ac90
Christophe Combelles <ccomb@free.fr>
2023-03-31 15:58:36 +0200

    WIP back-end

--- back/src/Main.hs
+++ back/src/Main.hs
@@ -1,35 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Concurrent (
-    Chan,
-    MVar,
-    dupChan,
-    forkIO,
-    newChan,
-    newMVar,
-    putMVar,
-    readChan,
-    takeMVar,
-    threadDelay,
-    writeChan,
- )
+import Control.Concurrent (Chan, MVar, dupChan, forkIO, newChan, newMVar, putMVar, readChan, takeMVar, threadDelay, writeChan)
 import Control.Exception (SomeException (SomeException), catch)
 import Control.Monad (forever, unless, when)
 import Control.Monad.Fix (fix)
-import qualified Data.Aeson as JSON (decode, encode)
-import qualified Data.Aeson.KeyMap as KeyMap
-import qualified Data.ByteString as BS (append)
+import Data.Aeson qualified as JSON (decode, encode)
+import Data.Aeson.KeyMap qualified as KeyMap
+import Data.ByteString qualified as BS (append)
 import Data.Function ((&))
-import qualified Data.List as List
-import qualified Data.Text as T (Text, append, split, unpack)
+import Data.List qualified as List
+import Data.Set as Set (Set, delete, empty, insert)
+import Data.Text qualified as T (Text, append, split, unpack)
 import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Data.Time.Clock.POSIX
-import Message (Message, appendMessage, getMessages, getMetaString, getUuids, isType, readMessages, setFlow)
+import Message (Message, appendMessage, getFlow, getMessages, getMetaString, getUuids, isType, readMessages, setFlow)
 import Network.HTTP.Types (status200)
-import qualified Network.Wai as Wai
-import qualified Network.Wai.Handler.Warp as Warp
+import Network.Wai qualified as Wai
+import Network.Wai.Handler.Warp qualified as Warp
 import Network.Wai.Handler.WebSockets (websocketsOr)
-import qualified Network.WebSockets as WS
+import Network.WebSockets qualified as WS
 import Options.Applicative
 
 -- dir, port, file
@@ -38,15 +27,16 @@ data Options = Options !String !Port !FilePath !Host !Port
 type NumClient = Int
 
 type Host = String
+
 type Port = Int
 
-newtype State = State {pending :: [Message]}
+newtype State = State {pending :: Set Message}
     deriving (Show)
 
 type StateMV = MVar State
 
 emptyState :: State
-emptyState = State{pending = []}
+emptyState = State{pending = Set.empty}
 
 options :: Parser Options
 options =
@@ -63,14 +53,14 @@ contentType filename = case reverse $ T.split (== '.') filename of
     "js" : _ -> "javascript"
     _ -> "raw"
 
-wsApp :: FilePath -> Chan (NumClient, Message) -> MVar NumClient -> WS.ServerApp
-wsApp msgPath chan ncMV pending_conn = do
+wsApp :: FilePath -> Chan (NumClient, Message) -> MVar NumClient -> StateMV -> WS.ServerApp
+wsApp msgPath chan ncMV stateMV pending_conn = do
     elmChan <- dupChan chan -- channel to the browser application, dedicated to a connection
     -- accept a new connexion
     conn <- WS.acceptRequest pending_conn
     -- increment the client number
     nc <- takeMVar ncMV
-    putMVar ncMV (nc + 1)
+    putMVar ncMV $! nc + 1
     putStrLn $ "\nBrowser " ++ show nc ++ " connected"
     -- wait for new messages coming from other connected browsers through the chan
     -- and send them to the currently connected browser
@@ -97,12 +87,27 @@ wsApp msgPath chan ncMV pending_conn = do
                     WS.Text bs _ -> WS.fromLazyByteString bs
                     WS.Binary bs -> WS.fromLazyByteString bs
                 ) of
-                Just msgs -> mapM_ (handleMessageFromBrowser msgPath conn nc elmChan) $ getMessages msgs
+                Just msgs -> mapM_ (handleMessageFromBrowser msgPath conn nc elmChan stateMV) $ getMessages msgs
                 Nothing -> sequence_ [putStrLn "\nError decoding incoming message"]
 
 clientApp :: FilePath -> Chan (NumClient, Message) -> StateMV -> WS.ClientApp ()
 clientApp msgPath storeChan stateMV conn = do
     putStrLn "Connected!"
+    -- Just reconnected, first send the pending messages to the Store
+    state <- takeMVar stateMV
+    putMVar stateMV $! state{pending = Set.empty}
+    catch
+        ( unless (null (pending state)) $ do
+            -- Send pending messages and store a flow=Sent version
+            let pendings = pending state
+            WS.sendTextData conn $ JSON.encode $ KeyMap.singleton "messages" pendings
+            mapM_ (appendMessage msgPath . setFlow "Sent") pendings
+        )
+        ( \(SomeException _) -> do
+            -- if something got wrong, put back the messages in the pending list
+            st <- takeMVar stateMV
+            putMVar stateMV $! state{pending = pending state <> pending st}
+        )
     -- TODO: Use the Flow to determine if it has been received by the store, in case the store was not alive.
     -- fork a thread to send back data from the channel to the central store
     _ <- forkIO $ do
@@ -110,7 +115,11 @@ clientApp msgPath storeChan stateMV conn = do
         forever $ do
             (n, ev) <- readChan storeChan
             unless (n == -1 || isType "InitiatedConnection" ev) $ do
-                putStrLn $ "\nSending back this message coming from browser " ++ show n ++ " to the Store: " ++ show ev
+                putStrLn $ "\nForwarding to the store this message coming from browser " ++ show n ++ ": " ++ show ev
+                -- Remove the current msg from the pending list
+                st <- takeMVar stateMV
+                putMVar stateMV $! st{pending = updatePending ev $ pending st}
+                -- send to the Store
                 WS.sendTextData conn $ JSON.encode $ KeyMap.singleton "messages" [ev]
 
     forever $ do
@@ -125,14 +134,30 @@ clientApp msgPath storeChan stateMV conn = do
             Just msgs -> mapM (handleMessageFromStore msgPath conn (-1) storeChan) msgs -- -1 is the Store. TODO use textual labels instead
             Nothing -> sequence [putStrLn "\nError decoding incoming message"]
 
-handleMessageFromBrowser :: FilePath -> WS.Connection -> NumClient -> Chan (NumClient, Message) -> Message -> IO ()
-handleMessageFromBrowser msgPath conn nc chan msg =
+update :: State -> Message -> State
+update state msg =
+    -- update the pending field
+    state{pending = updatePending msg $ pending state}
+
+updatePending :: Message -> Set Message -> Set Message
+updatePending msg pendings =
+    case getFlow msg of
+        Just "Requested" -> Set.insert msg pendings
+        Just "Sent" -> Set.insert msg pendings
+        Just "Processed" -> Set.delete msg pendings
+        _ -> pendings
+
+handleMessageFromBrowser :: FilePath -> WS.Connection -> NumClient -> Chan (NumClient, Message) -> StateMV -> Message -> IO ()
+handleMessageFromBrowser msgPath conn nc chan stateMV msg =
     do
         -- store the message in the message store
         unless (isType "InitiatedConnection" msg) $ do
             appendMessage msgPath msg
             WS.sendTextData conn $ JSON.encode $ KeyMap.singleton "messages" $ List.singleton (setFlow "Sent" msg)
             putStrLn $ "\nStored message and returned a Sent flow: " ++ show msg
+            -- Add it to the pending list (if Requested or Sent)
+            state <- takeMVar stateMV
+            putMVar stateMV $! update state msg
         -- if the message is a InitiatedConnection, get the uuid list from it,
         -- and send back all the missing messages (with an added ack)
         when (isType "InitiatedConnection" msg) $ do
@@ -215,9 +240,21 @@ serve (Options d p msgPath host port) = do
     stateMV <- newMVar emptyState
     storeChan <- dupChan chan -- output channel to the central message store
     firstTime <- getPOSIXTime
+    -- Reconstruct the state
+    putStrLn "Reconstructing the State"
+    msgs <- readMessages msgPath
+    state <- takeMVar stateMV
+    let newState = foldl update state msgs
+    putMVar stateMV newState
+    putStrLn "Old state:"
+    print state
+    putStrLn "New State:"
+    print newState
+    -- keep connection to the Store
     _ <- forkIO $ connectClient 1 firstTime host port msgPath storeChan stateMV
     putStrLn $ "Modelyz Studio, serving on http://localhost:" ++ show p ++ "/"
-    Warp.run p $ websocketsOr WS.defaultConnectionOptions (wsApp msgPath chan ncMV) $ httpApp (Options d p msgPath host port)
+    -- listen for client browsers
+    Warp.run p $ websocketsOr WS.defaultConnectionOptions (wsApp msgPath chan ncMV stateMV) $ httpApp (Options d p msgPath host port)
 
 main :: IO ()
 main =
