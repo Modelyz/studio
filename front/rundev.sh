543083258018c3bc13ff02aa3e1e2c6f02a03de4
Christophe Combelles <ccomb@free.fr>
2023-04-10 20:11:23 +0200

    fixed rundev

--- front/rundev.sh
+++ front/rundev.sh
@@ -2,10 +2,7 @@
 HERE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $HERE
 
-./build.sh dontstop
-# build the index (this is normally done in the docker entrypoint
-export WSS=ws://localhost:8080
-envsubst < src/index.html > ../build/index.html
+./build.sh
 
 npx elm-go -p 8080 -u -d ../build/ -s index.html src/Main.elm -- --output ../build/static/app.js
 
