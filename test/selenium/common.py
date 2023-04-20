4443e3b02bf8c13515ca04945c522814377f13b6
Christophe Combelles <ccomb@free.fr>
2023-04-20 23:27:40 +0200

    factored out some functions

---
+++ test/selenium/common.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+import os
+import subprocess
+import time
+
+ES = "messagestore.txt"
+site = "http://localhost:8080"
+
+is_running = lambda proc: proc.poll() is None
+
+
+def backends():
+    plist = []
+    if os.path.exists(ES):
+        os.remove(ES)
+    with subprocess.Popen(["../../build/studio", "-d", "../../build/"]) as proc:
+        plist.append(proc)
+        yield plist
+        for p in plist:
+            p.terminate()
+        for p in plist:
+            p.wait()
+
+
+def driver(nav):
+    if nav == "chrome":
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+    elif nav == "firefox":
+        service = FirefoxService(executable_path=GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service)
+    else:
+        raise NotImplementedError
+    return driver
+
+
+def count_evstore(string):
+    with open(ES) as es:
+        return es.read().count(string)
+
+
+def click(browser, text):
+    time.sleep(0.05)  # couldn't find a way to wait a detectable change
+    print("### click on " + text)
+    # WebDriverWait(browser, timeout=1).until(
+    #    cond.presence_of_all_elements_located((By.XPATH, f"//*[text()='{text}']"))
+    # )
+    return (
+        WebDriverWait(browser, timeout=1)
+        .until(lambda d: d.find_element(By.XPATH, f"//*[text()='{text}']"))
+        .click()
+    )
+
+
+def fill(browser, text):
+    return (
+        WebDriverWait(browser, timeout=1)
+        .until(lambda d: d.find_element(By.TAG_NAME, "input"))
+        .send_keys(text)
+    )
+
+
+def fill_by_id(browser, id_, text):
+    return (
+        WebDriverWait(browser, timeout=1)
+        .until(lambda d: d.find_element(By.ID, id_))
+        .send_keys(text)
+    )
+
+
+def find_by_id(browser, text):
+    return browser.find_element(By.ID, text)
+
+
+def find_by_text(browser, text):
+    return browser.find_element(By.XPATH, f"//*[contains(text(),'{text}')]")
+
+
+def text_exists(browser, text):
+    assert (
+        find_by_text(browser, text).text == text
+    ), f"The text '{text}' could not be found"
+
+
+def add_identifier_type(browser, scope, name):
+    open_url(browser, f"/identifier-type/add")
+    for s in scope:
+        click(browser, s)
+    click(browser, "Next →")
+    fill(browser, name)
+    click(browser, "Next →")
+    click(browser, "Next →")
+    click(browser, "Next →")
+    click(browser, "Free")
+    click(browser, "Validate and finish")
+
+
+def add_zone(browser, entity_route, zone, scope, fragments):
+    open_url(browser, f"/{entity_route}/add")
+    click(browser, zone)
+    for s in scope:
+        click(browser, s)
+    for f in fragments:
+        click(browser, f)
+    click(browser, "Name")
+    click(browser, "Validate and finish")
+
+
+def add_type(browser, vsegment, name, visible=True):
+    open_url(browser, f"/{vsegment}/add")
+    click(browser, "Next →")
+    fill(browser, name)
+    click(browser, "Next →")
+    click(browser, "Next →")
+    if not visible:
+        click(browser, "Display as a menu item")
+    click(browser, "Next →")
+    click(browser, "Validate and finish")
+
+
+def add_group_type(browser, name="", types=[], single=False, tree="Not", visible=True):
+    assert tree in (
+        "Not hierarchical",
+        "Hierarchical, an intermediate node can be selected",
+        "Hierarchical, a leaf must be selected",
+    )
+    open_url(browser, "/group-type/add")
+    for p in types:
+        click(browser, p)
+    click(browser, "Next →")
+    fill(browser, name)
+    click(browser, "Next →")
+    if not single:
+        click(browser, str(single))
+    click(browser, "Next →")
+    click(browser, tree)
+    click(browser, "Next →")
+    click(browser, "Next →")
+    if not visible:
+        click(browser, "Display as a menu item")
+    click(browser, "Next →")
+    click(browser, "Validate and finish")
+
+
+def add_event_type(
+    browser,
+    name,
+    provider,
+    receiver,
+    flow,
+    expression,
+    expfields,
+    visible=True,
+):
+    open_url(browser, "/event-type/add")
+    click(browser, "Next →")
+    fill(browser, name)
+    click(browser, "Next →")
+    click(browser, "Next →")
+    for s in provider:
+        click(browser, s)
+    click(browser, "Next →")
+    for s in receiver:
+        click(browser, s)
+    click(browser, "Next →")
+    for s in flow:
+        click(browser, s)
+    for e in expression:
+        click(browser, e)
+    for x, y in expfields.items():
+        fill_by_id(browser, x, y)
+    click(browser, "Next →")
+    if not visible:
+        click(browser, "Display as a menu item")
+    click(browser, "Next →")
+    click(browser, "Validate and finish")
+
+
+def add_process_type(browser, types=[], name="", eventTypes=[], visible=True):
+    open_url(browser, "/process-type/add")
+    for t in types:
+        click(browser, t)
+    click(browser, "Next →")
+    fill(browser, name)
+    click(browser, "Next →")
+    for e in eventTypes:
+        click(browser, e)
+    click(browser, "Next →")
+    click(browser, "Next →")
+    if not visible:
+        click(browser, "Display as a menu item")
+    click(browser, "Next →")
+    click(browser, "Validate and finish")
+
+
+def open_url(browser, path):
+    print("### open " + path)
+    browser.get(site + path)
--- test/selenium/test.py
+++ test/selenium/test.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support import expected_conditions as cond
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.service import Service as FirefoxService
+from common import (
+    backends,
+    add_identifier_type,
+    driver,
+    text_exists,
+    count_evstore,
+    open_url,
+)
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 import os
 import pytest
 import subprocess
@@ -20,85 +18,7 @@ site = "http://localhost:8080"
 is_running = lambda proc: proc.poll() is None
 
 
-@pytest.fixture
-def backends():
-    plist = []
-    if os.path.exists(ES):
-        os.remove(ES)
-    with subprocess.Popen(["../../build/studio", "-d", "../../build/"]) as proc:
-        plist.append(proc)
-        yield plist
-        for p in plist:
-            p.terminate()
-        for p in plist:
-            p.wait()
-
-
-def driver(nav):
-    if nav == "chrome":
-        service = ChromeService(executable_path=ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
-    elif nav == "firefox":
-        service = FirefoxService(executable_path=GeckoDriverManager().install())
-        driver = webdriver.Firefox(service=service)
-    else:
-        raise NotImplementedError
-    return driver
-
-
-def count_evstore(string):
-    with open(ES) as es:
-        return es.read().count(string)
-
-
-def click(browser, text):
-    time.sleep(0.1)  # couldn't find a way to wait a detectable change
-    # WebDriverWait(browser, timeout=1).until(
-    #    cond.presence_of_all_elements_located((By.XPATH, f"//*[text()='{text}']"))
-    # )
-    return (
-        WebDriverWait(browser, timeout=1)
-        .until(lambda d: d.find_element(By.XPATH, f"//*[text()='{text}']"))
-        .click()
-    )
-
-
-def fill(browser, text):
-    return (
-        WebDriverWait(browser, timeout=1)
-        .until(lambda d: d.find_element(By.TAG_NAME, "input"))
-        .send_keys(text + Keys.ENTER)
-    )
-
-
-def find_by_id(browser, text):
-    return browser.find_element(By.ID, text)
-
-
-def find_by_text(browser, text):
-    return browser.find_element(By.XPATH, f"//*[contains(text(),'{text}')]")
-
-
-def text_exists(browser, text):
-    assert (
-        find_by_text(browser, text).text == text
-    ), f"The text '{text}' could not be found"
-
-
-def add_resource_type(browser, name):
-    open_url(browser, "/identifier-type/add")
-    assert browser.title == "Adding an Identifier Type"
-    click(browser, "Resource Types")
-    click(browser, "Next →")
-    fill(browser, name)
-    click(browser, "Next →")
-    click(browser, "Next →")
-    click(browser, "Free")
-    click(browser, "Validate and finish")
-
-
-def open_url(browser, path):
-    browser.get(site + path)
+backends = pytest.fixture(backends)
 
 
 def test_sync_process_type(backends):
@@ -108,7 +28,7 @@ def test_sync_process_type(backends):
     chrome = driver("chrome")
     chrome.implicitly_wait(1)
 
-    add_resource_type(chrome, "AAAAAA")
+    add_identifier_type(chrome, ["Resource Types"], "AAAAAA")
     text_exists(chrome, "AAAAAA")
     WebDriverWait(chrome, 1).until(lambda _: os.path.exists(ES))
     assert count_evstore("AAAAAA") == 1, "(1) Wrong event count in the Event Store"
@@ -122,7 +42,7 @@ def test_sync_process_type(backends):
     text_exists(firefox, "AAAAAA")
 
     # Create another event from chrome and check it appears on firefox w/o reloading
-    add_resource_type(chrome, "BBBBBB")
+    add_identifier_type(chrome, ["Resource Types"], "BBBBBB")
     text_exists(chrome, "BBBBBB")
     text_exists(firefox, "BBBBBB")
 
@@ -133,13 +53,13 @@ def test_sync_process_type(backends):
     backends[-1].terminate()
     time.sleep(0.5)
 
-    add_resource_type(chrome, "CCCCCC")
+    add_identifier_type(chrome, ["Resource Types"], "CCCCCC")
     text_exists(chrome, "CCCCCC")
     time.sleep(0.5)
 
     backends.append(subprocess.Popen(["../../build/studio", "-d", "../../build/"]))
 
-    add_resource_type(chrome, "DDDDDD")
+    add_identifier_type(chrome, ["Resource Types"], "DDDDDD")
     text_exists(chrome, "DDDDDD")
     assert count_evstore("DDDDDD") == 1, "(2) Wrong event count in the Event Store"
     text_exists(firefox, "DDDDDD")
