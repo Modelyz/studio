32289149ca33a03acb06f5f495b92acf49d4ae78
Christophe Combelles <ccomb@free.fr>
2023-10-27 10:50:22 +0200

    renamed a variable for clarity

--- front/src/State.elm
+++ front/src/State.elm
@@ -133,7 +133,7 @@ aggregate ((Message m p) as msg) state =
         mid =
             messageId msg
 
-        prepState =
+        commonState =
             { state
                 | lastMessageTime = m.when
                 , pendingMessages = updatePending msg state.pendingMessages
@@ -147,18 +147,18 @@ aggregate ((Message m p) as msg) state =
         InitiatedConnection _ ->
             case m.flow of
                 Processed ->
-                    { prepState | syncing = False }
+                    { commonState | syncing = False }
 
                 _ ->
-                    prepState
+                    commonState
 
         AddedIdentifierType it ->
-            { prepState
+            { commonState
                 | identifierTypes = Dict.insert (IdentifierType.compare it) it state.identifierTypes
             }
 
         ChangedIdentifierType c ->
-            { prepState
+            { commonState
                 | identifierTypes =
                     Dict.insert (IdentifierType.compare c.new) c.new <|
                         Dict.remove (IdentifierType.compare c.old) state.identifierTypes
@@ -184,7 +184,7 @@ aggregate ((Message m p) as msg) state =
             }
 
         RemovedIdentifierType it ->
-            { prepState
+            { commonState
                 | identifierTypes = Dict.remove (IdentifierType.compare it) state.identifierTypes
                 , identifiers =
                     -- keep the identifiers whose name are different from the one removed,
@@ -195,12 +195,12 @@ aggregate ((Message m p) as msg) state =
             }
 
         AddedValueType it ->
-            { prepState
+            { commonState
                 | valueTypes = Dict.insert (ValueType.compare it) it state.valueTypes
             }
 
         ChangedValueType c ->
-            { prepState
+            { commonState
                 | valueTypes =
                     Dict.insert (ValueType.compare c.new) c.new <|
                         Dict.remove (ValueType.compare c.old) state.valueTypes
@@ -225,7 +225,7 @@ aggregate ((Message m p) as msg) state =
             }
 
         RemovedValueType vt ->
-            { prepState
+            { commonState
                 | valueTypes = Dict.remove (ValueType.compare vt) state.valueTypes
                 , values =
                     -- keep the values whose name are different from the one removed,
@@ -236,211 +236,211 @@ aggregate ((Message m p) as msg) state =
             }
 
         AddedValue v ->
-            { prepState
+            { commonState
                 | values = Dict.insert (Value.compare v) v state.values
             }
 
         AddedResourceType rt ->
-            { prepState
+            { commonState
                 | resourceTypes = Dict.insert (Uuid.toString rt.uuid) rt state.resourceTypes
                 , types = Dict.insert (Uuid.toString rt.uuid) ( rt.uuid, Type.HType HType.ResourceType, rt.parent ) state.types
             }
 
         RemovedResourceType uuid ->
-            { prepState
+            { commonState
                 | resourceTypes = Dict.remove (Uuid.toString uuid) state.resourceTypes
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedEventType et ->
-            { prepState
+            { commonState
                 | eventTypes = Dict.insert (Uuid.toString et.uuid) et state.eventTypes
                 , types = Dict.insert (Uuid.toString et.uuid) ( et.uuid, Type.HType HType.EventType, et.parent ) state.types
             }
 
         RemovedEventType uuid ->
-            { prepState
+            { commonState
                 | eventTypes = Dict.remove (Uuid.toString uuid) state.eventTypes
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedAgentType at ->
-            { prepState
+            { commonState
                 | agentTypes = Dict.insert (Uuid.toString at.uuid) at state.agentTypes
                 , types = Dict.insert (Uuid.toString at.uuid) ( at.uuid, Type.HType HType.AgentType, at.parent ) state.types
             }
 
         RemovedAgentType uuid ->
-            { prepState
+            { commonState
                 | agentTypes = Dict.remove (Uuid.toString uuid) state.agentTypes
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedCommitmentType cmt ->
-            { prepState
+            { commonState
                 | commitmentTypes = Dict.insert (Uuid.toString cmt.uuid) cmt state.commitmentTypes
                 , types = Dict.insert (Uuid.toString cmt.uuid) ( cmt.uuid, Type.HType HType.CommitmentType, cmt.parent ) state.types
             }
 
         RemovedCommitmentType uuid ->
-            { prepState
+            { commonState
                 | commitmentTypes = Dict.remove (Uuid.toString uuid) state.commitmentTypes
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedContractType cnt ->
-            { prepState
+            { commonState
                 | contractTypes = Dict.insert (Uuid.toString cnt.uuid) cnt state.contractTypes
                 , types = Dict.insert (Uuid.toString cnt.uuid) ( cnt.uuid, Type.HType HType.ContractType, cnt.parent ) state.types
             }
 
         RemovedContractType uuid ->
-            { prepState
+            { commonState
                 | contractTypes = Dict.remove (Uuid.toString uuid) state.contractTypes
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedProcessType pt ->
-            { prepState
+            { commonState
                 | processTypes = Dict.insert (Uuid.toString pt.uuid) pt state.processTypes
                 , types = Dict.insert (Uuid.toString pt.uuid) ( pt.uuid, Type.HType HType.ProcessType, pt.parent ) state.types
             }
 
         RemovedProcessType uuid ->
-            { prepState
+            { commonState
                 | processTypes = Dict.remove (Uuid.toString uuid) state.processTypes
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedResource r ->
-            { prepState
+            { commonState
                 | resources = Dict.insert (Uuid.toString r.uuid) r state.resources
                 , types = Dict.insert (Uuid.toString r.uuid) ( r.uuid, Type.TType TType.Resource, Just r.type_ ) state.types
             }
 
         RemovedResource uuid ->
-            { prepState
+            { commonState
                 | resources = Dict.remove (Uuid.toString uuid) state.resources
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedEvent e ->
-            { prepState
+            { commonState
                 | events = Dict.insert (Uuid.toString e.uuid) e state.events
                 , types = Dict.insert (Uuid.toString e.uuid) ( e.uuid, Type.TType TType.Event, Just e.type_ ) state.types
             }
 
         RemovedEvent uuid ->
-            { prepState
+            { commonState
                 | events = Dict.remove (Uuid.toString uuid) state.events
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedAgent a ->
-            { prepState
+            { commonState
                 | agents = Dict.insert (Uuid.toString a.uuid) a state.agents
                 , types = Dict.insert (Uuid.toString a.uuid) ( a.uuid, Type.TType TType.Agent, Just a.type_ ) state.types
             }
 
         RemovedAgent uuid ->
-            { prepState
+            { commonState
                 | agents = Dict.remove (Uuid.toString uuid) state.agents
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedCommitment cm ->
-            { prepState
+            { commonState
                 | commitments = Dict.insert (Uuid.toString cm.uuid) cm state.commitments
                 , types = Dict.insert (Uuid.toString cm.uuid) ( cm.uuid, Type.TType TType.Commitment, Just cm.type_ ) state.types
             }
 
         RemovedCommitment uuid ->
-            { prepState
+            { commonState
                 | commitments = Dict.remove (Uuid.toString uuid) state.commitments
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedContract cn ->
-            { prepState
+            { commonState
                 | contracts = Dict.insert (Uuid.toString cn.uuid) cn state.contracts
                 , types = Dict.insert (Uuid.toString cn.uuid) ( cn.uuid, Type.TType TType.Contract, Just cn.type_ ) state.types
             }
 
         RemovedContract uuid ->
-            { prepState
+            { commonState
                 | contracts = Dict.remove (Uuid.toString uuid) state.contracts
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedProcess pr ->
-            { prepState
+            { commonState
                 | processes = Dict.insert (Uuid.toString pr.uuid) pr state.processes
                 , types = Dict.insert (Uuid.toString pr.uuid) ( pr.uuid, Type.TType TType.Process, Just pr.type_ ) state.types
             }
 
         RemovedProcess uuid ->
-            { prepState
+            { commonState
                 | processes = Dict.remove (Uuid.toString uuid) state.processes
                 , reconciliations = Dict.filter (\_ r -> r.process /= uuid) state.reconciliations
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         AddedIdentifier ei ->
-            { prepState
+            { commonState
                 | identifiers = Dict.insert (Identifier.compare ei) ei state.identifiers
             }
 
         Configured conf ->
-            { prepState
+            { commonState
                 | configs = Dict.insert (Configuration.compare conf) conf state.configs
             }
 
         Unconfigured conf ->
-            { prepState
+            { commonState
                 | configs = Dict.remove (Configuration.compare conf) state.configs
             }
 
         AddedGroupType gt ->
-            { prepState
+            { commonState
                 | groupTypes = Dict.insert (Uuid.toString gt.uuid) gt state.groupTypes
                 , types = Dict.insert (Uuid.toString gt.uuid) ( gt.uuid, Type.HType HType.GroupType, gt.parent ) state.types
             }
 
         RemovedGroupType uuid ->
-            { prepState
+            { commonState
                 | groupTypes = Dict.remove (Uuid.toString uuid) state.groupTypes
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         DefinedGroup g ->
-            { prepState
+            { commonState
                 | groups = Dict.insert (Uuid.toString g.uuid) g state.groups
                 , types = Dict.insert (Uuid.toString g.uuid) ( g.uuid, Type.TType TType.Group, Just g.type_ ) state.types
             }
 
         RemovedGroup uuid ->
-            { prepState
+            { commonState
                 | groups = Dict.remove (Uuid.toString uuid) state.groups
                 , types = Dict.remove (Uuid.toString uuid) state.types
             }
 
         Grouped link ->
-            { prepState
+            { commonState
                 | grouped = Dict.insert (GroupLink.compare link) link state.grouped
             }
 
         Ungrouped link ->
-            { prepState
+            { commonState
                 | grouped = Dict.remove (GroupLink.compare link) state.grouped
             }
 
         Reconciled reconciliation ->
-            { prepState
+            { commonState
                 | reconciliations = Dict.insert (Reconcile.compare reconciliation) reconciliation state.reconciliations
             }
 
         Unreconciled reconciliation ->
-            { prepState
+            { commonState
                 | reconciliations = Dict.remove (Reconcile.compare reconciliation) state.reconciliations
             }
 
